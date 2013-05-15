73d36aee1d83ded2b2b6116edb89f4aa726612e8
palashsinharay <palash.sinharay2000@gmail.com>
2013-05-15 11:23:38 +0530

    implementation code seperated from class

--- ean.php
+++ ean.php
@@ -184,37 +184,7 @@ class ean {
 
 }
 
-//below is a example of ean class usage
-$EAN = new ean();
 
-$arrayInfo["city"] = 'mumbai';
-$arrayInfo['countryCode'] = 'IN';
-$arrayInfo['checkIn'] = "05/14/2013";
-$arrayInfo['checkOut'] = "05/15/2013";
-$arrayInfo['rooms'] = "room1=1,3&room2=1,5";
-$arrayInfo['numberOfResult'] = 10;
-            
-//$arrayInfo['propertyCategory'] = 1;
-//$arrayInfo['amenities'] = 1;
-
-$arrayInfo['maxStarRating']=5;
-$arrayInfo['minStarRating']=3;
-
-//$arrayInfo['minRate'] = 1000;
-//$arrayInfo['maxRate'] = 10000;
-            
-$arrayInfo['sort'] = "QUALITY";
-
-$data['hotelListPage_'.$arrayInfo['city']] = $EAN->HotelLists($arrayInfo);
-
-$arrayInfo['customerSessionId'] = $data['hotelListPage_'.$arrayInfo['city']]['HotelListResponse']['customerSessionId'];
-$arrayInfo['cacheKey'] = $data['hotelListPage_'.$arrayInfo['city']]['HotelListResponse']['cacheKey'];
-$arrayInfo['cacheLocation'] = $data['hotelListPage_'.$arrayInfo['city']]['HotelListResponse']['cacheLocation'];
-
-$data['hotelListPage_'.$arrayInfo['city'].'2'] = $EAN->HotelListsMore($arrayInfo);
-
-echo "<pre>";
-print_r($data);
 
 
 ?>
